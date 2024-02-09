924cd96487a1cd2e5a3c735fa8c0575a84b7f108
XYCode <xycode-xyc@outlook.com>
2024-02-09 19:00:48 +0800

    Autocreate runtime dir

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,20 @@
 import logging
 import configs
 import datetime
+import pathlib
 from rich.logging import RichHandler
 from rich.console import Console
 
-filehandler = logging.FileHandler(f'runtime/logs/{datetime.datetime.now().strftime("%Y-%m-%d")}.log')
+pathlib.Path('runtime/').mkdir(parents=True, exist_ok=True)
+pathlib.Path('runtime/logs').mkdir(parents=True, exist_ok=True)
+pathlib.Path('runtime/configs').mkdir(parents=True, exist_ok=True)
+
+filehandler = logging.FileHandler(
+    f'runtime/logs/{datetime.datetime.now().strftime("%Y-%m-%d")}.log')
 console = Console()
 
-filehandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+filehandler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'))
 
 logging.basicConfig(
     level=configs.LOG_LEVEL,
